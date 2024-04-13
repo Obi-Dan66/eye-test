19ab53f4ff0a144a0f01c4d37127c87c1265e20a
Obi-Dan66 <124266422+Obi-Dan66@users.noreply.github.com>
2024-04-13 18:02:44 +0200

    adding carousel logic to sharptest

--- eye-test/src/components/sharptest.jsx
+++ eye-test/src/components/sharptest.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+// sharptest.jsx
+import React, { useState } from 'react';
 
 const Sharptest = () => {
+  const [currentContent, setCurrentContent] = useState(0);
+  const totalContents = 6;
+
+  const handleNext = () => {
+    setCurrentContent((prevContent) => (prevContent + 1) % totalContents);
+  };
+
+  const getContent = (contentIndex) => {
+    switch (contentIndex) {
+      case 0:
+        return <h1>This is the Sharp Test Page</h1>;
+      case 1:
+        return <h1>Content 1</h1>;
+      case 2:
+        return <p>Content 2</p>;
+      case 3:
+        return <p>Content 3</p>;
+      case 4:
+        return <p>Content 4</p>;
+      case 5:
+        return <p>Content 5</p>;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div>
-      <h1>This is the Sharp Test Page</h1>
-      {/* Add your content for the Share Test page here */}
+      {getContent(currentContent)}
+      <button onClick={handleNext}>Next</button>
     </div>
   );
 }
