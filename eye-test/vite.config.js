e247dec918a05ac343a9910ea19d0b69fd4ac4cf
Obi-Dan66 <124266422+Obi-Dan66@users.noreply.github.com>
2024-05-20 16:27:57 +0200

    run build and deploy

--- eye-test/vite.config.js
+++ eye-test/vite.config.js
@@ -1,22 +1,8 @@
-   // vite.config.js
-   import { defineConfig } from 'vite';
-   import react from '@vitejs/plugin-react';
-   import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-   export default defineConfig({
-     base: '/eye-test/',  // Replace <repository-name> with your GitHub repo name
-     plugins: [react()],
-     resolve: {
-       alias: {
-         '@components': path.resolve(__dirname, 'src/components'),
-         // Add other aliases as needed
-       }
-     },
-     build: {
-       outDir: 'build',
-       assetsDir: 'assets',
-       rollupOptions: {
-         // Additional Rollup options can be specified here
-       }
-     }
-   });
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/eye-test/"
+})
