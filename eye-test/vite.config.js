42a7667a2ce9accd23926da97d24911995cbe18b
Obi-Dan66 <124266422+Obi-Dan66@users.noreply.github.com>
2024-05-20 15:03:55 +0200

    deploy

--- eye-test/vite.config.js
+++ eye-test/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: { outDir: 'build'
+    // `outDir` might be missing or set to 'dist'
+  }
 })
